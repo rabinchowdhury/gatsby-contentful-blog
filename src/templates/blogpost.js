c76f1a7bd2bb42c5f3e7cc6c5ee7edda074f576f
rabinchowdhury <rabinfed@gmail.com>
2019-06-30 22:54:13 -0400

    working blog

--- src/templates/blogpost.js
+++ src/templates/blogpost.js
@@ -2,44 +2,46 @@ import React from "react";
 import { Link, graphql } from "gatsby";
 import Layout from "../components/layout";
 import SEO from "../components/seo";
-const BlogPosts = ({ data }) => {
-  const blogPosts = data.allContentfulBlogPost.edges;
+
+const BlogPost = ({ data }) => {
+  const { title, body, image, tags } = data.contentfulBlogPost;
   return (
     <Layout>
-      <SEO title="Blog posts" />
-      <h1>{"Here's a list of all blogposts!"}</h1>
-      <div className="blogposts">
-        {blogPosts.map(({ node: post }) => (
-          <div key={post.id}>
-            <Link to={`/blogpost/${post.slug}`}>{post.title}</Link>
-          </div>
-        ))}
-        <span className="mgBtm__24" />
-        <Link to="/">Go back to the homepage</Link>
+      <SEO title={title} />
+      <div className="blogpost">
+        <h1>{title}</h1>
+        <img alt={title} src={image.file.url} />
+        <div className="tags">
+          {tags.map(tag => (
+            <span className="tag" key={tag}>
+              {tag}
+            </span>
+          ))}
+        </div>
+        <p className="body-text">{body.body}</p>
+        <Link to="/blogposts">View more posts</Link>
+        <Link to="/">Back to Home</Link>
       </div>
     </Layout>
   );
 };
-export default BlogPosts;
-export const query = graphql`
-  query BlogPostsPageQuery {
-    allContentfulBlogPost(limit: 1000) {
-      edges {
-        node {
-          id
-          title
-          slug
-          body {
-            body
-          }
-          image {
-            file {
-              url
-            }
-          }
-          tags
+
+export default BlogPost;
+
+export const pageQuery = graphql`
+  query($slug: String!) {
+    contentfulBlogPost(slug: { eq: $slug }) {
+      title
+      slug
+      body {
+        body
+      }
+      image {
+        file {
+          url
         }
       }
+      tags
     }
   }
 `;
